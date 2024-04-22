4af53c7f69e4ef5ec8bf411eeb01a59e1532c8ae
SoujanyaRegonda <soujanyaregonda234@gmail.com>
2024-04-22 12:11:46 +0000

    solution

--- src/main/java/com/example/player/service/PlayerH2Service.java
+++ src/main/java/com/example/player/service/PlayerH2Service.java
@@ -7,6 +7,8 @@ import com.example.player.model.PlayerRowMapper;
 import com.example.player.model.Player;
 import java.util.*;
 import com.example.player.repository.PlayerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class PlayerH2Service implements PlayerRepository {
@@ -21,23 +23,48 @@ public class PlayerH2Service implements PlayerRepository {
 
     @Override
     public Player getPlayerById(int playerId) {
-        return new Player(12, "prince", 24, "Bowler");
+        try {
+            return db.queryForObject("SELECT * FROM team WHERE playerId = ?", new PlayerRowMapper(), playerId);
+        }
+        catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        }
+        
     }
 
     @Override
     public Player addPlayer(Player player) {
-        return new Player(12, "prince", 24, "Bowler");
+        db.update("INSERT INTO team(playerName, jerseyNumber, role) values (?, ?, ?)", player.getPlayerName(), player.getJerseyNumber(), player.getRole());
+        return db.queryForObject("SELECT * FROM team WHERE playerName = ? and jerseyNumber = ?", new PlayerRowMapper(), player.getPlayerName(), player.getJerseyNumber());
+        
+
+
+        
 
     }
 
     @Override
     public Player updatePlayer(int playerId, Player player) {
-        return new Player(12, "prince", 24, "Bowler");
+        if (player.getPlayerName() != null) {
+            db.update("UPDATE team SET playerName = ? WHERE playerId = ?", player.getPlayerName(), playerId);
+
+        }
+        if (player.getJerseyNumber() != 0) {
+            db.update("UPDATE team SET jerseyNumber = ? WHERE playerId = ?", player.getJerseyNumber(), playerId);
+
+        }
+        if (player.getRole() != null) {
+            db.update("UPDATE team SET role = ? WHERE playerId = ?", player.getRole(), playerId);
+
+        }
+        return getPlayerById(playerId);
+        
 
     }
 
     @Override
     public void deletePlayer(int playerId) {
-
+        db.update("DELETE FROM team WHERE playerId = ?", playerId);
     }
 }
